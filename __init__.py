58b82e33cc995e4ef859e2ce5d49dd92473c86ef
xiuzhenpeng <xiuzhenpeng@iat-auto.com>
2025-07-16 14:40:55 +0800

    add __init

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ComfyUI-By-xiuzhenpeng
+"""
+
+from .xiu_iat import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# Export node mappings for ComfyUI
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
