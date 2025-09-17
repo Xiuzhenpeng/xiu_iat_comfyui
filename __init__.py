85a57c9faa84a6353afda43c05dba3b34f9155f5
xiuzhenpeng <1846357129@qq.com>
2025-09-17 10:14:40 +0800

    fix init error

--- __init__.py
+++ __init__.py
@@ -1,21 +1,7 @@
 """
 ComfyUI-By-xiuzhenpeng
 """
-from .xiu_iat import GetStringFromList
-from .select_ratio import SelectRatioToWH
-
-NODE_CLASS_MAPPINGS = {
-    # "OneStringToList": OneStringToList,
-    "GetStringFromList": GetStringFromList,
-    "SelectRatioToWH": SelectRatioToWH,
-}
-
-# A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {
-    # "OneStringToList": "OneStringToList Node",
-    "GetStringFromList": "GetStringFromList Node",
-    "SelectRatioToWH": "Generate Width And Height by Ratio",
-}
+from .xiu_iat import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 # Export node mappings for ComfyUI
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
